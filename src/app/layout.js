9265497b4be9a5e4296b15f14d641046bd78d40f
fachrurozi20 <fachrurozi283@gmail.com>
2026-02-05 16:12:03 +0700

    change favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Portfolio Fachrurozi",
+  title: "FACHRUROZI",
   description: "Personal portfolio built with Next.js",
 };
 
