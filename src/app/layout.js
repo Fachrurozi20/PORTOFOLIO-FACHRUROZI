0beeb359a512d4b09756e0dcdb8f501c52a0297b
fachrurozi20 <fachrurozi283@gmail.com>
2026-02-05 16:15:17 +0700

    change favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "FACHRUROZI",
-  description: "Personal portfolio built with Next.js",
+  description: "My Personal Portfolio",
 };
 
 export default function RootLayout({ children }) {
